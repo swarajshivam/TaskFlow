d9718839f69c8f4efa1ae11a964726512e48c5d2
swarajshivam <shivamswaraj48@gmail.com>
2026-07-06 23:52:48 +0530

    Complete user login API with bcrypt authentication

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -18,7 +18,7 @@ export const registerUser = async(req,res) => {
             message: "User already exists"
         });
     }
-    
+
     const hashedPassword = await bcrypt.hash(password,10);
 
     const user = await User.create({
@@ -33,6 +33,43 @@ export const registerUser = async(req,res) => {
             id: user._id,
             name: user.name,
             email:user.email
+        }
+    });
+};
+
+
+export const loginUser = async (req, res) => {
+
+    const { email, password} = req.body
+
+    if (!email || !password) {
+        return res.status(400).json({
+            message: "Please provide all fields"
+        });
+    }
+
+    const user = await User.findOne({email})
+
+    if(!user){
+        return res.status(400).json({
+            message: "Invalid Email or Password"
+        });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if(!isMatch){
+        return res.status(400).json({
+            message: "Invalid Email or Password"
+        });
+    }
+
+    return res.status(200).json({
+        message: "Login Successful",
+        user: {
+            id: user._id,
+            name: user.name,
+            email: user.email
         }
     });
 };
