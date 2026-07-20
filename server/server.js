c5d71fb39bfbc714beccb14d28e29ad1be6805ac
swarajshivam <shivamswaraj48@gmail.com>
2026-07-20 13:23:49 +0530

    Allow Vercel frontend in CORS

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://YOUR-VERCEL-APP.vercel.app"
+      "https://task-flow-lemon-omega.vercel.app/"
      ],
     credentials: true,
   })
